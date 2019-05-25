2acdaf25c72ed4f36092bf65dd5f1f198288925d
i-naji <teamnaji@gmail.com>
2019-05-25 19:50:51 +0430

    more readable api generator

--- generate/api/generator.py
+++ generate/api/generator.py
@@ -2,15 +2,13 @@ import os
 import re
 import shutil
 
-HOME = "generate/api"
-DESTINATION = "pytdlib/api"
-SECTION_RE = re.compile("---(\w+)---")
-COMBINATOR_RE = re.compile(r'^(\w+)\s(?:.*)=\s(\w+);$', re.MULTILINE)
-ARGS_RE = re.compile("(\w+):([\w<>]+)")
-DOCS_RE = re.compile(r"^//@")
-DOCS_RE1 = re.compile(r"^//-")
-DOCS_RE_2 = re.compile(r"^//@description")
-DOCS_RE_3 = re.compile(r"^//@class")
+HOME = 'generate/api'
+DESTINATION = 'pytdlib/api'
+SECTION_RE = re.compile(r'---(\w+)---')
+COMBINATOR_RE = re.compile(r'^(\w+) (.*)? = (\w+);$')
+ARGS_RE = re.compile('(\w+):([\w<>]+)')
+DOCS_RE = re.compile(r'//((-)|@)(description)?([\w -.,;]+)(@?)(.*)?')
+ClASS_DOCS_RE = re.compile(r"^//@class (\w+) @description (.*)")
 
 core_types = {
     'int': 'int', 'int32': 'int', 'int53': 'int', 'int64': 'int', 'long': 'int', 'double': 'float',
@@ -35,25 +33,29 @@ def snek(s: str):
 
 
 class Combinator:
-    def __init__(self, section: str, name: str, doc: list, args: list, return_type: str):
+    def __init__(self, section, name, description):
         self.section = section
         self.name = name
-        self.doc = doc
+        self.description = description
+
+
+class MethodCombinator(Combinator):
+    def __init__(self, name: str, description: str, params_desc: list, args: dict, return_type: str, section='types'):
+        super().__init__(section, name, description)
+        self.params_description = params_desc
         self.args = args
         self.return_type = return_type
 
 
-class ClassCombinator:
-    def __init__(self, name, doc):
-        self.section = "types"
-        self.name = name
-        self.doc = doc
+class ClassCombinator(Combinator):
+    def __init__(self, name, description):
+        super().__init__('types', name, description)
         self.sub_classes = set()
 
 
 def start():
-    shutil.rmtree("{}/types/".format(DESTINATION), ignore_errors=True)
-    shutil.rmtree("{}/functions/".format(DESTINATION), ignore_errors=True)
+    shutil.rmtree('{}/types/'.format(DESTINATION), ignore_errors=True)
+    shutil.rmtree('{}/functions/'.format(DESTINATION), ignore_errors=True)
 
     with open('{}/scheme/td_api.tl'.format(HOME), encoding='utf-8') as f:
         scheme = f.read()
@@ -61,83 +63,73 @@ def start():
     with open("{}/template/class.txt".format(HOME), encoding="utf-8") as f:
         template = f.read()
 
-    notice = ""
+    notice = ''
 
-    section = "types"
-    class_name = ""
-    combinators = []
+    section = 'types'
+    method_description = ''
+    method_combinators = {}
     class_combinators = {}
     descriptions = []
     scheme_data = scheme.splitlines()
     return_types = set()
+
     for line in scheme_data[14:]:
+        # check section changer line
         s = SECTION_RE.match(line)
         if s:
-            section = "functions"
+            section = s.group(1)
             continue
 
-        document = DOCS_RE.match(line)
-        if document:
+        # check for class description line
+        c = ClASS_DOCS_RE.match(line)
+        if c:
+            class_name, class_desc = c.groups()
+            class_combinators[class_name] = ClassCombinator(class_name, class_desc)
+            continue
 
-            cls = DOCS_RE_3.match(line)
-            if cls:
-                class_name = line.split('@class ', 1)[1].split(' ', 1)[0]
-                doc = line.split('@description ', 1)[1]
-                class_combinators[class_name] = ClassCombinator(class_name, doc)
-                continue
-
-            start_doc = DOCS_RE_2.match(line)
-            if start_doc:
-                line = line.replace('description ', '', 1)
-
-            line = line[3:]
-
-            description = line.split('@')
-            for des in description:
-                descriptions.append(des)
-                continue
-        more_doc = DOCS_RE1.match(line)
-        if more_doc:
-            line = line[3:]
-            more_description = line.split('@')
-            if more_description:
+        # check description line
+        d = DOCS_RE.match(line)
+        if d:
+            _, extra_desc, method_desc, desc, has_extra, extra_data = d.groups()
+            # check for method description
+            if method_desc is not None:
+                method_description = desc
+            elif extra_desc is not None:
                 if descriptions:
-                    descriptions[-1] += more_description[0]
+                    descriptions[-1] += '. %s' % desc
                 else:
-                    class_combinators[class_name].doc += more_description[0]
-                for d in more_description[1:]:
-                    descriptions.append(d)
-            else:
-                if descriptions:
-                    descriptions[-1] += line
-                else:
-                    class_combinators[class_name].doc += line
+                    method_description += '. %s' % desc
+
+            # split data by @ and save in descriptions list
+
+            if has_extra:
+                descriptions.extend(des for des in extra_data.split('@'))
+            continue
 
         combinator = COMBINATOR_RE.match(line)
         if combinator:
-            name, return_type = combinator.groups()
-            args = ARGS_RE.findall(line)
-            combinators.append(
-                Combinator(
-                    section,
-                    upper_first(name),
-                    descriptions,
-                    args,
-                    return_type,
-                )
+            name, args_text, return_type = combinator.groups()
+            args = {arg.split(':')[0]: arg.split(':')[1] for arg in args_text.split(' ')}
+            method_combinators[name] = MethodCombinator(
+                upper_first(name),
+                method_description,
+                descriptions,
+                args,
+                return_type,
+                section=section
             )
 
-            if section == "types" and return_type in class_combinators:
-                class_combinators[return_type].sub_classes.add(name)
-            if section == "functions":
+            if section == 'types':
+                if return_type in class_combinators:
+                    class_combinators[return_type].sub_classes.add(name)
+            else:
                 return_types.add(return_type)
+            method_description = ''
             descriptions = []
-    for combinator in class_combinators.values():
-        combinators.append(combinator)
 
-    total = len(combinators)
+    total = len(method_combinators) + len(class_combinators)
     current = 0
-    for c in combinators:
+    for c_name, c in {**class_combinators, **method_combinators}.items():
         print('Generating [{0:03}%] {name}'.format(
             round(current * 100 / total), name=c.name
         ), end='                                        \r', flush=True)
@@ -153,25 +145,22 @@ def start():
         with open(init, "a", encoding="utf-8") as f:
             f.write("from .{} import {}\n".format(snek(c.name), c.name))
 
-        extra, has_extra = (", extra=None", "self.extra = extra") if c.section == "functions" else ("", "")
-
+        imports = []
+        doc_args = [c.description, '\n\n    Attributes:\n        ID (:obj:`str`): ``', c.name, '``\n\n    Args']
+        slots = []
+        arguments = []
+        fields = []
+        return_arguments = []
         if isinstance(c, ClassCombinator):
-            doc_args = str(c.doc) + "\n\n    No parameters required."
-            imports = ""
-            read_args = 'if q.get("@type"):\n            return Object.read(q)'
-            slots = ''
-            arguments = ""
-            fields = "pass"
-            return_arguments = ""
-            return_read = " or ".join(upper_first(r_name) for r_name in c.sub_classes)
-        else:
-            doc_args = []
-            read_args = []
-            fields = []
-            imports = ""
+            doc_args.append('\n        No parameters required.\n\n')
+            return_read = [upper_first(r_name) for r_name in c.sub_classes]
+            read_args = ['if q.get("@type"):', '    return Object.read(q)']
 
-            for i, arg in enumerate(c.args):
-                arg_name, arg_type = arg
+        else:
+
+            read_args = []
+            pre_arg_doc = []
+            for arg_name, arg_type in c.args.items():
 
                 if arg_type in core_types:
                     field_type = core_types[arg_type]
@@ -223,7 +212,7 @@ def start():
                     # imports += "from .{} import {}\n".format(snek(arg_type), field_type)
                     arg_type = "(:class:`pytdlib.api.types.{}`)".format(arg_type)
 
-                doc_args.append(
+                pre_arg_doc.append(
                     "{} {}".format(
                         arg_name, arg_type,
                     )
@@ -232,66 +221,56 @@ def start():
                 fields.append(
                     "self.{0} = {0}  # {1}".format(arg_name, field_type)
                 )
-
                 read_args.append(
-                    "{} = {}".format(
-                        arg_name, arg_read,
-                    )
+                    "{} = {}".format(arg_name, arg_read)
                 )
 
-            if doc_args:
-                doc_args = "Args:\n        " + "\n        ".join(
-                    "{}:\n            {}".format(
-                        doc_args[i],
-                        ". ".join(
-                            darg[1:] if darg.startswith(' ') else darg
-                            for darg in c.doc[i + 1].split(' ', 1)[1].split('.'))
-                    ) for i in range(len(doc_args)))
-                read_args = "\n        ".join(read_args)
+            if pre_arg_doc:
+
+                arguments = [arg_name for arg_name in c.args.keys()]
+                return_arguments = c.args.keys()
+
+                for arg_name, arg_desc in dict(zip(pre_arg_doc, c.params_description)).items():
+                    doc_args.append('\n        ')
+                    doc_args.append(arg_name)
+                    for arg_d in arg_desc.split('.'):
+                        doc_args.append('\n            ')
+                        doc_args.append(arg_d)
+
             else:
-                doc_args = "No parameters required."
-                read_args = ""
+                doc_args.append('\n        No parameters required.\n\n')
 
-            doc_args = c.doc[0] + "\n\n    Attributes:\n        ID (:obj:`str`): ``{}``\n\n    ".format(
-                c.name) + doc_args
-            doc_args += "\n\n    Returns:\n        " + c.return_type
-            doc_args += "\n\n    Raises:\n        :class:`pytdlib.Error`"
+            doc_args.append('\n\n    Returns:\n        ')
+            doc_args.append(c.return_type)
+            doc_args.append('\n\n    Raises:\n        ')
+            doc_args.append(':class:`pytdlib.Error`')
 
-            slots = ", ".join(
-                [repr(i[0]) for i in c.args]
-            ) if c.args else ""
+            slots.extend(repr(i) for i in c.args.keys())
 
-            arguments = ", " + ", ".join(
-                [i[0] for i in c.args]
-            ) if c.args else ""
+            return_read = [c.name]
 
-            fields = "\n        ".join(
-                fields
-            ) if fields else "pass"
+            if c.section != 'types':
+                slots.append('\'extra\'')
+                arguments.append('extra')
+                fields.append("self.extra = extra")
 
-            return_arguments = ", ".join(
-                [i[0] for i in c.args]
-            ) if c.args else ""
-
-            return_read = c.name
+            if arguments:
+                arguments.append('')
 
         with open("{}/{}/{}.py".format(DESTINATION, c.section, snek(c.name)), "w+", encoding="utf-8") as f:
             f.write(
                 template.format(
                     notice=notice,
-                    imports=imports,
-                    slots=slots,
-                    s_extra=extra and ((slots and ', ' or '') + '\'extra\'') or '',
-                    extra=extra,
-                    has_extra=has_extra,
-                    docstring=doc_args,
+                    imports='\n'.join(imports),
                     class_name=c.name,
-                    ID=lower_first(c.name),
-                    arguments=arguments,
-                    fields=fields,
-                    return_read=return_read,
-                    read=read_args,
-                    return_arguments=return_arguments
+                    docstring=''.join(doc_args),
+                    slots=', '.join(slots),
+                    ID=c_name,
+                    arguments=', '.join(arguments),
+                    fields='\n        '.join(fields) or 'pass',
+                    return_read=' or '.join(return_read),
+                    read='\n        '.join(read_args),
+                    return_arguments=', '.join(return_arguments)
                 )
             )
     with open("{}/utils/all_types.py".format(DESTINATION), "w", encoding="utf-8") as f:
@@ -299,9 +278,8 @@ def start():
         f.write(notice + "\n\n")
         f.write("all_types = {")
 
-        for c in combinators:
-            if c.section == "types":
-                f.write("\n    \"{0}\": \"{1}\",".format(c.name, snek(c.name)))
+        for c in filter(lambda x: x.section == "types", method_combinators.values()):
+            f.write("\n    \"{0}\": \"{1}\",".format(c.name, snek(c.name)))
         f.write("\n}\n")
 
 
