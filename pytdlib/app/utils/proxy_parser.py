77fe4d7f2ed16f91ce1bdf2cae3c7e6fcce902a5
i-naji <teamnaji@gmail.com>
2019-06-02 09:37:43 +0430

    add proxy parser

---
+++ pytdlib/app/utils/proxy_parser.py
@@ -0,0 +1,19 @@
+from pytdlib.api import types
+from pytdlib.app.types.proxy import Socks5Proxy, HttpProxy, MTprotoProxy
+
+
+def proxy_parser(proxy):
+    kwargs = dict(
+        id=proxy.id,
+        hostname=proxy.server,
+        port=proxy.port,
+        last_used_date=proxy.last_used_date,
+        enabled=proxy.is_enabled
+    )
+    proxies = {
+        types.ProxyTypeSocks5: lambda p: (Socks5Proxy, dict(username=p.username, password=p.password)),
+        types.ProxyTypeHttp: lambda p: (HttpProxy, dict(username=p.username, password=p.password, http_only=p.http_only)),
+        types.ProxyTypeMtproto: lambda p: (MTprotoProxy, dict(secret=p.secret))
+    }
+    p_type, ex_kwargs = proxies[type(proxy.type)](proxy.type)
+    return p_type(**{**kwargs, **ex_kwargs})
