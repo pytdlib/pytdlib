a2fc39275b44c24f9b7e8401ebf0ba29c4e61a61
i-naji <teamnaji@gmail.com>
2019-06-02 09:28:19 +0430

    add object_to_bytes function and more make TDclient class loggy

--- pytdlib/td/client/client.py
+++ pytdlib/td/client/client.py
@@ -1,9 +1,9 @@
 from ctypes import CDLL
-try:
-    import ujson as json
-except ImportError:
-    import json
 from .json_client import TDJsonClient
+from pytdlib.utils import object_to_bytes, json
+import logging
+
+log = logging.getLogger(__name__)
 
 
 class TDClient:
@@ -18,17 +18,18 @@ class TDClient:
             self._json_client = json_client
         else:
             raise ValueError("At least one of tdjson or json_client argument is required")
-        self.client_id = None if client_id is not None else client_id
+        self.client_id = self.create() if client_id is None else client_id
+        log.info('TDClient(%s) Created' % self.client_id)
 
     @property
     def _tdjson(self)-> "CDLL":
         return self._json_client._tdjson
 
-    def create(self, force: bool=False):
-        if self.client_id is not None and not force:
-            return
-        self.client_id = self._json_client.create()
-        return self.client_id
+    def create(self, use: bool=False):
+        client_id = self._json_client.create()
+        if use:
+            self.client_id = client_id
+        return client_id
 
     def receive(self, timeout: float = 1.0):
         result = self._json_client.receive(self.client_id, timeout)
@@ -38,27 +39,13 @@ class TDClient:
 
     def send(self, query: str or dict or bytes):
 
-        if isinstance(query, str):
-            query = query.encode('utf-8')
-        elif isinstance(query, dict):
-            query = json.dumps(query).encode('utf-8')
-        elif hasattr(query, "__bytes__"):
-            query = bytes(query)
-        elif not isinstance(query, bytes):
-            return
+        query = object_to_bytes(query)
 
         self._json_client.send(self.client_id, query)
 
     def execute(self, query: str or dict or bytes):
 
-        if isinstance(query, str):
-            query = query.encode('utf-8')
-        elif isinstance(query, dict):
-            query = json.dumps(query).encode('utf-8')
-        elif hasattr(query, "__bytes__"):
-            query = bytes(query)
-        elif not isinstance(query, bytes):
-            return
+        query = object_to_bytes(query)
 
         result = self._json_client.execute(self.client_id, query)
         if result:
@@ -66,7 +53,11 @@ class TDClient:
         return result
 
     def destroy(self):
+        log.info('TDClient(%s) Destroyed' % self.client_id)
         return self._json_client.destroy(self.client_id)
+
+    def __str__(self):
+        return 'TDClient(%s)' % self.client_id
 
     def __del__(self):
         if self.client_id is not None:
--- pytdlib/utils/__init__.py
+++ pytdlib/utils/__init__.py
@@ -1,1 +1,21 @@
+import logging
 from .authorization_stats import authorization_stats
+
+log = logging.getLogger(__name__)
+
+try:
+    import ujson as json
+    log.info("Using ujson")
+except ImportError:
+    import json
+
+
+def object_to_bytes(query):
+    if isinstance(query, bytes):
+        return query
+    elif isinstance(query, dict):
+        return json.dumps(query).encode('utf-8')
+    elif isinstance(query, str):
+        return query.encode('utf-8')
+    else:
+        return bytes(query)
