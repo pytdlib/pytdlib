ee6ae753d2cf377e4db16da2e2fac53839e17a54
i-naji <teamnaji@gmail.com>
2019-06-02 09:24:42 +0430

    add new_client and new_app methods to TD class

--- pytdlib/td/td.py
+++ pytdlib/td/td.py
@@ -1,12 +1,13 @@
 from ctypes import CDLL
 from .log import TDLog
 from .client import TDClient
+from ..app import Telegram
 
 
 class TD:
 
     def __init__(self, td_lib: str or CDLL, log: TDLog=None, client: TDClient=None):
-        self.td_lib = td_lib
+        self.td_lib = td_lib if isinstance(td_lib, CDLL) else CDLL(td_lib)
         self._client = client if client is not None else TDClient(self.td_lib)
         self._log = log if log is not None else TDLog(self.td_lib)
 
@@ -17,6 +18,12 @@ class TD:
     @property
     def client(self) -> "TDClient":
         return self._client
+
+    def new_client(self, client_id: int=None):
+        return TDClient(self.td_lib, client_id=client_id)
+
+    def new_app(self, tdjson: str or CDLL=None, **kwargs):
+        return Telegram(tdjson=tdjson if tdjson is not None else self.td_lib, **kwargs)
 
     def close(self):
         self.client.__del__()
